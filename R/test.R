ca19c0063043d9e7e5fb8173f6b9e369a4b1cd8d
palmercd <cameron.palmer@nih.gov>
2020-10-05 15:55:25 -0400

    docs(r/test.r): add placeholder test function

---
+++ R/test.R
@@ -0,0 +1,12 @@
+#' Test function for gitlab communication
+#'
+#' @description
+#' `hello.world` prints said text to screen.
+#'
+#' @details
+#' This is entirely a placeholder and will be removed.
+#'
+
+hello.world <- function(ignored) {
+    print("hello world")
+}
