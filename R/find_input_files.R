b125d9241389a5f1913c18eeb2887c366a870d92
palmercd <cameron.palmer@nih.gov>
2020-10-06 11:23:04 -0400

    feat(r/find_input_files.r): add support for input file detection and pairing of controls and tests
    adds a function that scans a directory for formatted input filenames ([A-Z0-9]_[A-Z]_Telo.txt or
    *_36B4.txt) and pairs them together appropriately for later parsing
    
    re #1

---
+++ R/find_input_files.R
@@ -0,0 +1,39 @@
+#' Acquire input text files in (experiment, control) pairs
+#'
+#' @description
+#' `find.input.files` scans a given directory for structured output from
+#' a lab telomere experiment and organizes them by experiment, control pairs.
+#'
+#' @details
+#' This function aggregates filenames and performs mild error checking on 
+#' the existence of expected pairs of filenames. It will ignore any file
+#' present in the specified directory that does not match the expected general
+#' filename format. Paths should be specified using the R convention of "/" as the
+#' delimiter between directories (as opposed to the Windows convention of "\").
+#' This function should not be called directly by the end user.
+#'
+#' @param path Character vector describing path to input file directory
+#' @param telo.tag Character vector describing file tag for experimental condition
+#' @param control.tag Character vector describing file tag for control condition
+#' @return list of pairs of (telomere, control) files
+#' @keywords telomeres
+#' @export
+#' @examples
+#' find.input.files("/path/to/Data/Exports")
+find.input.files <- function(path, telo.tag = "Telo", control.tag = "36B4") {
+    telo.files <- list.files(path, paste("^[A-Z0-9]+_[A-Z]_", telo.tag, "\\.txt$", sep=""))
+    control.files <- list.files(path, paste("^[A-Z0-9]+_[A-Z]_", control.tag, "\\.txt$", sep=""))
+    stopifnot(length(telo.files) == length(control.files))
+    lapply(1:length(telo.files), function(i) {
+        ## paired files appear to be named [A-Z]+([0-9]+)_([A-Z])_36B4.txt and
+        ##   [A-Z]+{\1+1}_\2_Telo.txt
+        expected.telo <- paste(substr(control.files[i], 1, 2), 
+                               as.numeric(gsub("^[A-Z]+([0-9]+)_.*$", "\\1", control.files[i])) + 1, 
+                               gsub("^[A-Z0-9]+(_[A-Z]_).*$", "\\1", control.files[i]), 
+                               telo.tag, 
+                               ".txt",
+                               sep = "")
+        stopifnot(telo.files[i] == expected.telo)
+        c(telo.files[i], control.files[i])}
+	)
+}
