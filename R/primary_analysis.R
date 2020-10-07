457526acf89f38882151f09f8a0c4f9f33c9360b
palmercd <cameron.palmer@nih.gov>
2020-10-07 15:14:07 -0400

    feat(r/primary_analysis.r): add skeleton class for primary analysis results
    adds an S4 class that will eventually contain the contents of each Data/Analysis/*.xlsx "Analysis"
    tab. just the barebones class definition for the moment
    
    re #6

---
+++ R/primary_analysis.R
@@ -0,0 +1,50 @@
+#' Store primary analysis results, processed from ExportDatum
+#'
+#' @slot Source.Well.ID factor of well IDs from source data
+#' @slot Internal.Control boolean vector of whether a sample is an internal control
+#' @slot Rep1.Well,Rep2.Well,Rep3.Well factor of 384 well plate locations
+#' @slot Rep1.ExperimentalCt.prefilter,Rep2.ExperimentalCt.prefilter,Rep3.ExperimentalCt.prefilter numeric vector of raw telomeric Ct values
+#' @slot Rep1.ControlCt.prefilter,Rep2.ControlCt.prefilter,Rep3.ControlCt.prefilter numeric vector of raw control Ct values
+#' @slot Rep1.ExperimentalCt.postfilter,Rep2.ExperimentalCt.postfilter,Rep3.ExperimentalCt.postfilter numeric vector of post-filtering telomeric Ct values
+#' @slot Rep1.ControlCt.postfilter,Rep2.ControlCt.postfilter,Rep3.ControlCt.postfilter numeric vector of post-filtering control Ct values
+#' @slot Avg.ExperimentalCt numeric vector of mean post-filtering telomeric Ct values
+#' @slot SD.ExperimentalCt numeric vector of standard deviation of post-filtering telomeric Ct values
+#' @slot PerCV.ExperimentalCt numeric vector of [something] of post-filtering telomeric Ct values
+#' @slot Avg.ControlCt numeric vector of mean post-filtering control Ct values
+#' @slot SD.ControlCt numeric vector of standard deviation of post-filtering control Ct values
+#' @slot PerCV.ControlCt numeric vector of [something] of post-filtering control Ct values
+#' @slot Fit.ExperimentalConc numeric vector of exponential fit values from standard curve for telomeric data
+#' @slot Fit.ControlConc numeric vector of exponential fit values from standard curve for control data
+#' @slot TS.Ratio numeric vector of ratio of telomeric to control fit
+#' @slot Normalized.TS numeric vector of TS ratio normalized to [something from controls]
+#' @keywords telomeres
+#' @examples
+#' new("PrimaryAnalysis")
+#' 
+setClass("PrimaryAnalysis", slots = list(Source.Well.ID = "factor",
+                                         Internal.Control = "vector",
+                                         Rep1.Well = "factor",
+                                         Rep2.Well = "factor",
+                                         Rep3.Well = "factor",
+                                         Rep1.ExperimentalCt.prefilter = "vector",
+                                         Rep2.ExperimentalCt.prefilter = "vector",
+                                         Rep3.ExperimentalCt.prefilter = "vector",
+                                         Rep1.ControlCt.prefilter = "vector",
+                                         Rep2.ControlCt.prefilter = "vector",
+                                         Rep3.ControlCt.prefilter = "vector",
+                                         Rep1.ExperimentalCt.postfilter = "vector",
+                                         Rep2.ExperimentalCt.postfilter = "vector",
+                                         Rep3.ExperimentalCt.postfilter = "vector",
+                                         Rep1.ControlCt.postfilter = "vector",
+                                         Rep2.ControlCt.postfilter = "vector",
+                                         Rep3.ControlCt.postfilter = "vector",
+                                         Avg.ExperimentalCt = "vector",
+                                         SD.ExperimentalCt = "vector",
+                                         PerCV.ExperimentalCt = "vector",
+                                         Avg.ControlCt = "vector",
+                                         SD.ControlCt = "vector",
+                                         PerCV.ControlCt = "vector",
+                                         Fit.ExperimentalConc = "vector",
+                                         Fit.ControlConc = "vector",
+                                         TS.Ratio = "vector",
+                                         Normalized.TS = "vector"))
