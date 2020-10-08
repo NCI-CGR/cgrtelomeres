e7d8c54e3aa89faf7e9848a072276f7efcbef5cf
palmercd <cameron.palmer@nih.gov>
2020-10-08 12:24:48 -0400

    feat(r/primary_analysis.r): add most processing for PrimaryAnalysis construction from ExportDatum
    This adds most features required to create the data in Data/Analysis/*xlsx. Some steps are left
    outstanding due to the lack of resolution of the copied source data tab. Most of this is not yet
    properly tested.
    
    re #6

--- R/primary_analysis.R
+++ R/primary_analysis.R
@@ -23,6 +23,8 @@
 #' @slot Avg.ControlCt numeric vector of mean post-filtering control Ct values
 #' @slot SD.ControlCt numeric vector of standard deviation of post-filtering control Ct values
 #' @slot PerCV.ControlCt numeric vector of [something] of post-filtering control Ct values
+#' @slot Model.Experiment object of class lm, fit model for telomeric data
+#' @slot Model.Control object of class lm, fit model for control data
 #' @slot Fit.ExperimentalConc numeric vector of exponential fit values from standard curve for telomeric data
 #' @slot Fit.ControlConc numeric vector of exponential fit values from standard curve for control data
 #' @slot TS.Ratio numeric vector of ratio of telomeric to control fit
@@ -54,6 +56,8 @@ setClass("PrimaryAnalysis", slots = list(Source.Well.ID = "factor",
                                          Avg.ControlCt = "vector",
                                          SD.ControlCt = "vector",
                                          PerCV.ControlCt = "vector",
+                                         Model.Experiment = "lm",
+                                         Model.Control = "lm",
                                          Fit.ExperimentalConc = "vector",
                                          Fit.ControlConc = "vector",
                                          TS.Ratio = "vector",
@@ -61,13 +65,13 @@ setClass("PrimaryAnalysis", slots = list(Source.Well.ID = "factor",
 
 #' Constructor for primary analysis class
 #'
-#' Initializes replicate well assignments to default 384 well plate layout.
+#' Initializes replicate well assignments to default 384 well plate layout
+#' and standard configuration of 96 well source.
 #' Note that other slots in class can be set but must be named, and named
 #' replacements to well assignments will be overwritten, so if you want to
 #' use nonstandard well assignments, construct the class directly with new()
 #'
 #' @keywords telomeres
-#' @export
 #' @examples
 #' PrimaryAnalysis(Source.Well.ID = rep("", 309))
 #'
@@ -83,5 +87,171 @@ PrimaryAnalysis <- function(...) {
     obj@Rep3.Well <- factor(c(paste(LETTERS[seq(2, by = 2, length.out = 7)], rep(6, 7), sep = ""),
                               paste(LETTERS[seq(2, by = 2, length.out = 8)], rep(seq(1, 23, 2), each = 8), sep = "")),
                             levels = shared.levels)
+    obj@Source.Well.ID <- factor(c(rep("Standards", 6),
+                                   "NTC",
+                                   paste(rep(LETTERS[1:8], 12), sprintf("%02d", rep(1:12, each = 8)), sep = "-")))
+}
+
+#' Apply Ct filtering to PrimaryAnalysis intermediates based on some criteria
+#'
+#' @description
+#' `apply.Ct.filtering` takes raw Ct(Cp) values and applies mild postprocessing
+#' to conform with current behavior of the analysis protocol.
+#'
+#' @details
+#' The current filter applied is "Ct equal to 38 is set to NA". This is different
+#' than what's applied in the excel template (equal to 28, set to 0), but based on
+#' how the Cts are defined, the 28 criterion is actually a bug in the template. By
+#' inspection, it seems like the actual current protocol is to go through and
+#' manually blank entries that exceed some Ct threshold, possibly closer to 30.
+#' This function provides centralized control should that filter need to be
+#' adjusted in the future.
+#' 
+#' @param vec Numeric vector of raw Ct values.
+#' @return Numeric vector of input with some overlarge values set to NA.
+#' @keywords telomeres
+#' @seealso [read.export.datum()] for acquiring raw Ct values, [PrimaryAnalysis()]
+#' for construction of PrimaryAnalysis objects that use these processed Cts.
+#' @examples
+#' filtered.data <- apply.Ct.filtering(raw.data)
+#'
+apply.Ct.filtering <- function(vec) {
+    res <- vec
+    res[abs(res - 38) < 1.5e-8] <- NA
+    res
+}
+
+#' Compute replicate mean with restrictions
+#'
+#' @description
+#' `process.average` takes three replicates' data and combines vector-wise
+#' into means while allowing a single observation from each condition at
+#' most to be missing (flagged as NA).
+#'
+#' @details
+#' There is an implicit restriction that appears to be applied manually to the
+#' real data example, where conditions with fewer than two replicates are blanked.
+#' Hopefully, this should cover that situation, while allowing single dropouts.
+#'
+#' @param vec.rep1 numeric vector of first replicate Ct data
+#' @param vec.rep2 numeric vector of second replicate Ct data
+#' @param vec.rep3 numeric vector of third replicate Ct data
+#' @return numeric vector of vector-wise averages, with NA exclusion criteria applied
+#' @seealso [process.standard.deviation()] for equivalent calculation with standard deviation.
+#' @examples
+#' vec.mean <- process.average(rnorm(10), rnorm(10), rnorm(10))
+#' 
+process.average <- function(vec.rep1,
+                            vec.rep2,
+                            vec.rep3) {
+    apply(cbind(vec.rep1, vec.rep2, vec.rep3),
+          1,
+          function (i) {
+              ifelse(length(which(!is.na(i))) < 2, mean(i, na.rm = TRUE), NA)
+          })
+}
+
+#' Compute replicate standard deviation with restrictions
+#'
+#' @description
+#' `process.standard.deviation` takes three replicates' data and combines vector-wise
+#' into standard deviations while allowing a single observation from each condition
+#' at most to be missing (flagged as NA).
+#'
+#' @details
+#' There is an implicit restriction that appears to be applied manually to the
+#' real data example, where conditions with fewer than two replicates are blanked.
+#' Hopefully, this should cover that situation, while allowing single dropouts.
+#'
+#' @param vec.rep1 numeric vector of first replicate Ct data
+#' @param vec.rep2 numeric vector of second replicate Ct data
+#' @param vec.rep3 numeric vector of third replicate Ct data
+#' @return numeric vector of vector-wise averages, with NA exclusion criteria applied
+#' @seealso [process.average()] for equivalent calculation with mean
+#' @examples
+#' vec.sd <- process.standard.deviation(rnorm(10), rnorm(10), rnorm(10))
+#' 
+process.standard.deviation <- function(vec.rep1,
+                                       vec.rep2,
+                                       vec.rep3) {
+    apply(cbind(vec.rep1, vec.rep2, vec.rep3),
+          1,
+          function (i) {
+              ifelse(length(which(!is.na(i))) < 2, sd(i, na.rm = TRUE), NA)
+          })
+}
+
+#' Create a PrimaryAnalysis from a loaded ExportDatum
+#'
+#' @description
+#' `create.analysis` takes an ExportDatum object containing telomeric
+#' and control data and applies the standard analysis currently being run
+#' by the excel template that generates files in Data/Analysis/*xlsx.
+#'
+#' @details
+#' This function attempts to emulate the results in the primary analysis spreadsheet.
+#' There's a fair amount of redundant information (log2 transform of some data, linear
+#' regression information that is deprecated) that is not being included for the
+#' moment. 
+#'
+#' @param export.datum an ExportDatum object with raw qPCR data for processing
+#' @return a PrimaryAnalysis object containing the processed results for the input
+#' @keywords telomeres
+#' @export
+#' @seealso [PrimaryAnalysis()] for constructing empty PrimaryAnalysis objects;
+#' [read.export.datum()] for generating compatible input data structures.
+#' @examples
+#'
+create.analysis <- function(export.datum) {
+    obj <- PrimaryAnalysis()
+    ## load Cp data from export datum at the appropriate wells
+    obj@Rep1.ExperimentalCt.prefilter <- export.datum@Cp.Telo[obj@Rep1.Well]
+    obj@Rep2.ExperimentalCt.prefilter <- export.datum@Cp.Telo[obj@Rep2.Well]
+    obj@Rep3.ExperimentalCt.prefilter <- export.datum@Cp.Telo[obj@Rep3.Well]
+    obj@Rep1.ControlCt.prefilter <- export.datum@Cp.Control[obj@Rep1.Well]
+    obj@Rep2.ControlCt.prefilter <- export.datum@Cp.Control[obj@Rep2.Well]
+    obj@Rep3.ControlCt.prefilter <- export.datum@Cp.Control[obj@Rep3.Well]
+    ## apply filtering based on *some criterion*
+    obj@Rep1.ExperimentalCt.postfilter <- apply.Ct.filtering(obj@Rep1.ExperimentalCt.prefilter)
+    obj@Rep2.ExperimentalCt.postfilter <- apply.Ct.filtering(obj@Rep2.ExperimentalCt.prefilter)
+    obj@Rep3.ExperimentalCt.postfilter <- apply.Ct.filtering(obj@Rep3.ExperimentalCt.prefilter)
+    obj@Rep1.ControlCt.postfilter <- apply.Ct.filtering(obj@Rep1.ControlCt.prefilter)
+    obj@Rep2.ControlCt.postfilter <- apply.Ct.filtering(obj@Rep2.ControlCt.prefilter)
+    obj@Rep3.ControlCt.postfilter <- apply.Ct.filtering(obj@Rep3.ControlCt.prefilter)
+    ## compute average, SD, percent coefficient of variation for each condition.
+    ## just by inspection, it seems like conditions with fewer than two successful
+    ## replicates are blanked out.
+    obj@Avg.ExperimentalCt <- process.average(obj@Rep1.ExperimentalCt.postfilter,
+                                              obj@Rep2.ExperimentalCt.postfilter,
+                                              obj@Rep3.ExperimentalCt.postfilter)
+    obj@SD.ExperimentalCt <- process.standard.deviation(obj@Rep1.ExperimentalCt.postfilter,
+                                                        obj@Rep2.ExperimentalCt.postfilter,
+                                                        obj@Rep3.ExperimentalCt.postfilter)
+    obj@PerCV.ExperimentalCt <- 100 * obj@SD.ExperimentalCt / obj@Avg.ExperimentalCt
+    obj@Avg.ControlCt <- process.average(obj@Rep1.ControlCt.postfilter,
+                                         obj@Rep2.ControlCt.postfilter,
+                                         obj@Rep3.ControlCt.postfilter)
+    obj@SD.ControlCt <- process.standard.deviation(obj@Rep1.ControlCt.postfilter,
+                                                   obj@Rep2.ControlCt.postfilter,
+                                                   obj@Rep3.ControlCt.postfilter)
+    obj@PerCV.ControlCt <- 100 * obj@SD.ControlCt / obj@Avg.ControlCt
+    ## must run exponential regressions against the standard lanes.
+    ## now, it *appears* that the standard concentrations are predefined in the excel template,
+    ## and are likely fixed across experiments. set these here for now; possibly expose them later.
+    ## note that this model requires post hoc transformation before you get the exact coefficients
+    ## that were being produced with excel's `logest`
+    fixed.concentrations <- c(4, 1.6, 0.64, 0.256, 0.1024, 0.041)
+    obj@Model.Experiment <- lm(log2(obj@Avg.ExperimentalCt[1:6]) ~ log2(fixed.concentrations))
+    obj@Model.Control <- lm(log2(obj@Avg.ControlCt[1:6]) ~ log2(fixed.concentrations))
+    ## get predicted concentrations using transformed fit data from models
+    obj@Fit.ExperimentalConc <- 2^((log2(obj@Avg.ExperimentalCt) - summary(obj@Model.Experiment)$coeff[1,1]) / summary(obj@Model.Experiment)$coeff[2,1])
+    obj@Fit.ControlConc <- 2^((log2(obj@Avg.ExperimentalCt) - summary(obj@Model.Experiment)$coeff[1,1]) / summary(obj@Model.Experiment)$coeff[2,1])
+    ## compute T/S ratio as simple division of above
+    obj@TS.Ratio <- obj@Fit.ExperimentalConc / obj@Fit.ControlConc
+    ## normalization divides the above T/S ratio by the average T/S ratio of internal control samples.
+    ## without pasted sample data, I don't believe this is currently available information.
+    ## so for the moment, just add placeholder values.
+    obj@Normalized.TS <- obj@TS.Ratio
+    ## that's it?
     obj
 }
