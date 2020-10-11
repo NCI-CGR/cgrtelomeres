0cebdb50f7af7f6c8ed8082ceab5fbd5dc36ee3f
palmercd <cameron.palmer@nih.gov>
2020-10-11 10:38:46 -0400

    feat(r/main.r): add primary interface options for source plate contents and list
    though the test case can potentially work around it, in real deployment subject information and well
    identification need to be pulled from separate LIMS annotation files. this patches the relevant
    parameters into the primary entry point and adds documentation and some switches for test case use,
    along with their type checking. this patch does *not* add any associated functionality.
    
    re #2 #5 #6 #12

--- R/main.R
+++ R/main.R
@@ -10,22 +10,59 @@
 #' directory will be created if it doesn't already exist. Any existing conflicting
 #' output files will be overwritten by this method, so please change output.path
 #' if you don't want this behavior.
+#'
+#' During development, plate content report and list files were not necessarily
+#' available for test cases. As such, an override mode has been added
+#' that pulls pre-pasted copies of relevant content report data from the
+#' spreadsheets in Data/Analysis from an existing telomere run. During
+#' deployment, this will almost certainly not be the desired behavior, and
+#' this feature is scheduled to be removed. This flag will also be overridden
+#' when plate list and content report are both specified.
+#'
+#' In all existing test datasets, a fixed mapping from 96 well to 384 well
+#' layouts has been found. For mild efficiency purposes, this mapping
+#' has been set as the default. If there is ever any doubt, or if the
+#' well mapping is changed at some point in the future, setting
+#' `infer.384.locations` to `FALSE` will trigger dynamic remapping using
+#' the data in `plate.content.report` and `plate.list`.
 #' 
 #' @param input.path Character vector of input directory path
 #' @param output.path Character vector of output directory path
-#' @param project.id Character vector of project ID (e.g. "GP0317-TL7")
+#' @param project.id Character vector of project ID (e.g. "GP0317-TL7") or NA
+#' @param plate.content.report Character vector of plate content report for project (e.g. "PlateContentReport_GP0317-TL1.xls") or NA
+#' @param plate.list Character vector of plate list for project (e.g. "PlateList_GP0317-TL1.xls") or NA
+#' @param infer.384.locations Logical: whether to assume fixed 96->384 well mapping
+#' @param subject.list.from.input.path Logical: whether to pull subject data from Data/Analysis/*xlsx of an existing analysis run
+#' @return TBD
 #' @keywords telomeres
 #' @export
 #' @examples
 #' process.experiment("Examples for Bioinformatics", "C:/Users/palmercd/Documents/telomeres/output", "GP0317-TL7")
 #'
-process.experiment <- function(input.path, output.path, project.id) {
+process.experiment <- function(input.path,
+                               output.path,
+                               project.id = NA,
+                               plate.content.report = NA,
+                               plate.list = NA,
+                               infer.384.locations = FALSE,
+                               subject.list.from.input.path = TRUE) {
     ## check parameter requirements
-    stopifnot(is.vector(input.path, mode = "character"))
+    ## type checks
+    stopifnot(is.vector(input.path, mode = "character") & dir.exists(input.path))
     stopifnot(is.vector(output.path, mode = "character"))
-    stopifnot(is.vector(project.id, mode = "character"))
-    ## check input directory path exists
-    stopifnot(dir.exists(input.path))
+    stopifnot(is.vector(project.id, mode = "character") | isTRUE(is.na(project.id)))
+    stopifnot((is.vector(plate.content.report, mode = "character") &
+               file.exists(plate.content.report)) |
+              isTRUE(is.na(plate.content.report)))
+    stopifnot((is.vector(plate.list, mode = "character") &
+               file.exists(plate.list)) |
+              isTRUE(is.na(plate.list)))
+    stopifnot(is.logical(infer.384.locations))
+    stopifnot(is.logical(subject.list.from.input.path))
+    ## `!subject.list.from.input.path` only works if the plate list and content report are both specified
+    stopifnot((!is.na(plate.content.report) & !is.na(plate.list)) | subject.list.from.input.path)
+    ## `infer.384.locations` only works if the plate list and content report are both specified
+    stopifnot(!infer.384.locations | (infer.384.locations & !is.na(plate.content.report) & !is.na(plate.list)))
     ## if output directory path does not exist, create it
     create.output.directories(output.path)
     ## aggregate pairs of filenames from input.path/Data/Exports
