42a707299087453a297e96beb839d0d9d18d2487
palmercd <cameron.palmer@nih.gov>
2020-10-14 10:25:06 -0400

    feat(r/export_datum.r): add source content extraction from plate content and list files
    the existing telomere analysis protocol has the user manually copy in some entries from a plate
    contents file into an excel template. I was not originally provided the required contents (and later
    plate list) file for initial testing, so I initially left some fields blank and assumed fixed
    96->384 well linking, and later pulled in a copy of the pasted data from the existing test output.
    this patch adds the interface, parameter testing, and implementation for extracting the actual
    content information from the source contents file if provided; it can fall back to the earlier
    workaround methods, which is good because I still don't have the contents and list files
    corresponding to my test case lol
    
    re #5 #12

--- R/export_datum.R
+++ R/export_datum.R
@@ -39,20 +39,34 @@ setClass("ExportDatum", slots = list(Analysis.Code = "vector",
 #' existing spreadsheets. This will be removed at a future date, but allows development
 #' and testing to continue for the time being.
 #'
-#' @param exp.control.filenames length-2 vector of filenames: one for Telo, one for 36B4
-#' @param source.plate.contents.search.path charaver vector with path to test case Data/Analysis *.xlsx files
-#' @return an instance of S4 class `ExportDatum` containing aligned relevant information.
+#' @param exp.control.filenames Length-2 vector of filenames: one for Telo, one for 36B4
+#' @param source.plate.contents.search.path Character vector with path to test case Data/Analysis *.xlsx files
+#' @param subject.list.from.input.path Logical: whether to pull subject data from Data/Analysis/*xlsx of an existing analysis run
+#' @param plate.content.report Character vector of plate content report for project (e.g. "PlateContentReport_GP0317-TL1.xls") or NA
+#' @param plate.list Character vector of plate list for project (e.g. "PlateList_GP0317-TL1.xls") or NA
+#' @return An instance of S4 class `ExportDatum` containing aligned relevant information.
 #' @keywords telomeres
 #' @seealso [find.input.files()] for generating the expected input for this function.
 #' @examples
 #' read.export.datum(c("Data/Exports/PC29625_A_Telo.txt", "Data/Exports/PC29624_A_36B4.txt"))
 #' 
-read.export.datum <- function(exp.control.filenames, source.plate.contents.search.path) {
+read.export.datum <- function(exp.control.filenames,
+                              source.plate.contents.search.path = NA,
+                              subject.list.from.input.path = NA,
+                              plate.content.report = NA,
+                              plate.list = NA) {
+    ## assorted input checks
     stopifnot(is.vector(exp.control.filenames, mode = "character"))
     stopifnot(length(exp.control.filenames) == 2)
+    stopifnot(is.vector(source.plate.contents.search.path, mode = "character") | isTRUE(is.na(source.plate.contents.search.path)))
+    stopifnot(is.logical(subject.list.from.input.path) | isTRUE(is.na(subject.list.from.input.path)))
+    stopifnot(is.vector(plate.content.report, mode = "character") | isTRUE(is.na(plate.content.report)))
+    stopifnot(is.vector(plate.list, mode = "character") | isTRUE(is.na(plate.list)))
+    ## read exported data
     exp.data <- read.table(exp.control.filenames[1], sep = "\t", skip = 1, header = TRUE)
     control.data <- read.table(exp.control.filenames[2], sep = "\t", skip = 1, header = TRUE)
     stopifnot(identical(exp.data$Pos, control.data$Pos))
+    ## set '0' entries to NA, as that's actually the correct interpretation
     control.data$Standard[control.data$Standard < 2e-16] <- NA
     obj <- new("ExportDatum",
                Analysis.Code = gsub("^.*/[A-Z0-9]+_([A-Z])_.*$", "\\1", exp.control.filenames[1]),
@@ -64,18 +78,43 @@ read.export.datum <- function(exp.control.filenames, source.plate.contents.searc
     names(obj@Standard) <- exp.data$Pos
 
     ## deal with Source Plate Contents
-    stopifnot(is.vector(source.plate.contents.search.path, mode = "character"))
-    stopifnot(dir.exists(source.plate.contents.search.path))
-    target.source.file <- list.files(source.plate.contents.search.path, paste("^.+_", obj@Analysis.Code, "\\.xlsx$", sep = ""))
-    stopifnot(is.vector(target.source.file, mode = "character"))
-    stopifnot(length(target.source.file) == 1)
-    source.plate.contents <- openxlsx::read.xlsx(paste(source.plate.contents.search.path, target.source.file, sep = "/"), sheet = 1, rowNames = FALSE, colNames = TRUE)
-    stopifnot(length(which(colnames(source.plate.contents) == "Well.ID")) == 1)
-    stopifnot(length(which(colnames(source.plate.contents) == "Vial.ID")) == 1)
-    source.plate.contents[, "Well.ID"] <- as.vector(source.plate.contents[, "Well.ID"], mode = "character")
-    source.plate.contents[, "Vial.ID"] <- as.vector(source.plate.contents[, "Vial.ID"], mode = "character")
-    obj@Well.ID <- source.plate.contents[, "Well.ID"]
-    obj@Vial.ID <- source.plate.contents[, "Vial.ID"]
+    ## if the caller requests subject list information pulled from an existing Data/Analysis/*xlsx file
+    if (isTRUE(!is.na(subject.list.from.input.path)) & subject.list.from.input.path & isTRUE(!is.na(source.plate.contents.search.path))) {
+        stopifnot(dir.exists(source.plate.contents.search.path))
+        target.source.file <- list.files(source.plate.contents.search.path, paste("^.+_", obj@Analysis.Code, "\\.xlsx$", sep = ""))
+        stopifnot(is.vector(target.source.file, mode = "character"))
+        stopifnot(length(target.source.file) == 1)
+        source.plate.contents <- openxlsx::read.xlsx(paste(source.plate.contents.search.path, target.source.file, sep = "/"), sheet = 1, rowNames = FALSE, colNames = TRUE)
+        stopifnot(length(which(colnames(source.plate.contents) == "Well.ID")) == 1)
+        stopifnot(length(which(colnames(source.plate.contents) == "Vial.ID")) == 1)
+        source.plate.contents[, "Well.ID"] <- as.vector(source.plate.contents[, "Well.ID"], mode = "character")
+        source.plate.contents[, "Vial.ID"] <- as.vector(source.plate.contents[, "Vial.ID"], mode = "character")
+        obj@Well.ID <- source.plate.contents[, "Well.ID"]
+        obj@Vial.ID <- source.plate.contents[, "Vial.ID"]
+    } else if (isTRUE(!is.na(plate.content.report)) & isTRUE(!is.na(plate.list))) {
+        ## the caller provides enough information to pull data from a plate content report and list
+        plate.content.data <- openxlsx::read.xlsx(plate.content.report, sheet = 1, rowNames = FALSE, colNames = TRUE)
+        plate.list.data <- openxlsx::read.xlsx(plate.list, sheet = 1, rowNames = FALSE, colNames = TRUE)
+        ## pending further example datasets, the current link is: content file column "Description" will have
+        ## an entry "{Project.Code}_Intermediate_{Analysis.Code}". That row's corresponding "Identifier" entry
+        ## links to the plate content file column "Plate.ID", and then you want the content file entries
+        ## "Well.ID" and "Vial.ID" as above
+        stopifnot(length(which(colnames(plate.list.data) == "Description")) == 1)
+        stopifnot(length(which(colnames(plate.list.data) == "Identifier")) == 1)
+        stopifnot(length(which(colnames(plate.content.data) == "Plate.ID")) == 1)
+        stopifnot(length(which(colnames(plate.content.data) == "Well.ID")) == 1)
+        stopifnot(length(which(colnames(plate.content.data) == "Vial.ID")) == 1)
+        matched.index <- which(grepl(paste("^.*_Intermediate_", obj@Analysis.Code, "$", sep = ""), plate.list.data$Description))
+        stopifnot(length(matched.index) == 1)
+        stopifnot(length(which(plate.content.data$Plate.ID == plate.list.data$Identifier[matched.index])) > 0)
+        target.contents <- plate.content.data[plate.content.data$Plate.ID == plate.list.data$Identifier[matched.index],]
+        target.contents$Well.ID <- as.vector(target.contents$Well.ID, mode = "character")
+        target.contents <- target.contents[order(target.contents$Well.ID),]
+        obj@Well.ID <- target.contents$Well.ID
+        obj@Vial.ID <- target.contents$Vial.ID
+    } else {
+        stop("In read.export.datum(), no valid method for pulling source plate contents detected")
+    }
     ## end dealing with Source Plate Contents
     
     obj
